20b14615d2cfa1ebf46651c8dfac9e3074e65bd7
Raphael Serota <raphael.serota@gmail.com>
2020-08-30 01:48:06 -0500

    dont detach for db tasks

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 npm install && \
 docker-compose up --build -d && \
-docker-compose exec -d web npm run create-tables && \
+docker-compose exec -T web npm run create-tables && \
 echo 'Running any new migrations' && \
 docker-compose exec -T web npx knex migrate:latest
 
