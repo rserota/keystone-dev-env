18659c792df16845ca7288acc35ad8b2998b0059
Raphael Serota <raphael.serota@gmail.com>
2020-08-31 13:07:19 -0500

    check if tables are initialised

--- scripts/init.js
+++ scripts/init.js
@@ -2,17 +2,22 @@ const child_process = require("child_process");
 const exec = function(command, options) {
 	return child_process.execSync(command, { encoding: "utf8", ...options });
 };
-console.log(exec("ls ./db/db_data | wc -w"));
 
-exec("npm install", { stdio: "inherit" });
-console.log(exec("ls ./db/db_data | wc -w"));
 exec("docker-compose up --build -d", { stdio: "inherit" });
-try {
-	exec("docker-compose exec -T web npm run create-tables", {
-		stdio: "inherit",
-	});
-} catch (e) {
-	console.log("create-tables did not run");
+exec("npm install", { stdio: "inherit" });
+const dbTables = exec(
+	"docker-compose exec -T db psql -U postgres -d postgres -c '\\dt'"
+); // query the db container to check if the 'Todo' table exists
+if (!dbTables.includes("Todo")) {
+	try {
+		exec("docker-compose exec -T web npm run create-tables", {
+			stdio: "inherit",
+		});
+	} catch (e) {
+		console.log("create-tables did not run");
+	}
+} else {
+	console.log("Tables have already been initialised");
 }
 console.log("Running any new migrations");
 exec("docker-compose exec -T web npx knex migrate:latest", {
